93943f5d443230260cbc6fc4b8af5ef03851f806
gitshawn <shawn@nulogix.net>
2021-01-12 09:01:11 -0500

    Create bootstrap-arch.sh

---
+++ bootstrap-arch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+# Inspired by download.sh file from package:
+# Arch Linux Install Script (alis) by picodotdev
+# Downloads other scripts and conf files from my repo and executes alis.sh
+
+curl -O https://raw.githubusercontent.com/gitshawn/dotfiles/main/alis.conf
+curl -O https://raw.githubusercontent.com/gitshawn/dotfiles/main/alis.sh
+curl -O https://raw.githubusercontent.com/gitshawn/dotfiles/main/alis-reboot.sh
+
+chmod +x alis.sh
+chmod +x alis-reboot.sh
+
+./alis.sh
