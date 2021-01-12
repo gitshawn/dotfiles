4111d19243b3a36caa0b3bb738dad9e7afb331ee
gitshawn <shawn@nulogix.net>
2021-01-12 09:12:33 -0500

    Update after-alis.sh

--- after-alis.sh
+++ after-alis.sh
@@ -6,12 +6,15 @@ fancy_echo() {
   printf "\\n - = - $fmt\\n" "$@"
 }
 
-fancy_echo "Install X, openbox and related packages" 
+fancy_echo "Install X and related packages" 
 pacman -S --noconfirm \
 xorg-server \
 xorg-xinit \
 noto-fonts noto-fonts-emoji noto-fonts-extra noto-fonts-cjk \
-ttf-fira-code \ 
+ttf-fira-code
+
+fancy_echo "Install openbox and related packages" 
+pacman -S --noconfirm \
 openbox \
 obconf \
 feh \
