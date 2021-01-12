140259bec7c68a324b2a5cbcc1a498dd13c961f6
gitshawn <shawn@nulogix.net>
2021-01-12 08:34:20 -0500

    Create after-alis.sh

---
+++ after-alis.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+pacman -S --noconfirm \
+xorg-server \
+xorg-xinit \
+noto-fonts noto-fonts-emoji noto-fonts-extra noto-fonts-cjk \
+ttf-fira-code \ 
+openbox \
+obconf \
+feh \
+maim \
+volumeicon \
+tint2 \
+alacritty
