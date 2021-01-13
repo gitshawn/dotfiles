54a54844cc6bc3808d11e1d9dc662f00c2c0098e
gitshawn <shawn@nulogix.net>
2021-01-13 05:42:15 -0500

    Update after-alis.sh

--- after-alis.sh
+++ after-alis.sh
@@ -7,14 +7,14 @@ fancy_echo() {
 }
 
 fancy_echo "Install X and related packages" 
-pacman -S --noconfirm \
+sudo pacman -S --noconfirm \
 xorg-server \
 xorg-xinit \
 noto-fonts noto-fonts-emoji noto-fonts-extra noto-fonts-cjk \
 ttf-fira-code
 
 fancy_echo "Install openbox and related packages" 
-pacman -S --noconfirm \
+sudo pacman -S --noconfirm \
 openbox \
 obconf \
 feh \
@@ -22,10 +22,15 @@ volumeicon \
 tint2
 
 fancy_echo "Install system utils" 
-pacman -S --noconfirm \
+sudo pacman -S --noconfirm \
 alacritty \
 maim \
 git 
+
+fancy_echo "Install obmenu-generator (AUR)"
+git clone https://aur.archlinux.org/obmenu-generator.git
+cd obmenu-generator
+makepkg -si
 
 fancy_echo "Get my dotfiles"
 git clone https://github.com/gitshawn/dotfiles.git
