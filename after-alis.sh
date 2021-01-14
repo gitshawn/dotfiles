b487a432492761aacf0d661b6594823b44cd87ee
gitshawn <shawn@nulogix.net>
2021-01-14 08:30:49 -0500

    Update after-alis.sh

--- after-alis.sh
+++ after-alis.sh
@@ -6,37 +6,37 @@ fancy_echo() {
   printf "\\n - = - $fmt\\n" "$@"
 }
 
-fancy_echo "Install X and related packages" 
+fancy_echo "Install pacman packages" 
 sudo pacman -S --noconfirm \
 xorg-server \
 xorg-xinit \
 noto-fonts noto-fonts-emoji noto-fonts-extra noto-fonts-cjk \
 ttf-fira-code
-
-fancy_echo "Install openbox and related packages" 
-sudo pacman -S --noconfirm \
 openbox \
 obconf \
 feh \
+lightdm \
+light-locker \
+oblogout \
+xdotool \
 volumeicon \
-tint2
-
-fancy_echo "Install system utils" 
-sudo pacman -S --noconfirm \
-alacritty \
+tint2 \
+rxvt-unicode \
+rofi \
 maim \
-git 
+git \
+firefox-developer-edition
 
-fancy_echo "Install obmenu-generator (AUR)"
-git clone https://aur.archlinux.org/obmenu-generator.git
-cd obmenu-generator
-makepkg -si
+# fancy_echo "Install obmenu-generator (AUR)"
+# git clone https://aur.archlinux.org/obmenu-generator.git
+# cd obmenu-generator
+# makepkg -si
 
-fancy_echo "Get my dotfiles"
+fancy_echo "Get my dotfiles and copy them here"
 git clone https://github.com/gitshawn/dotfiles.git
-mv dotfiles/.xinitrc ~
+cp dotfiles/.* ~/
 mkdir -p ~/.config
 cp -r dotfiles/.config/* ~/.config
 rm -rf dotfiles
 
-fancy_echo "Apply post install configs"
+# fancy_echo "Apply post install configs"
