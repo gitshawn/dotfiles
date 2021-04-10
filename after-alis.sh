2100c9b6366426a1e8b4605c097fbe330544ae0b
gitshawn <shawn@nulogix.net>
2021-04-10 06:57:37 -0400

    Update after-alis.sh

--- after-alis.sh
+++ after-alis.sh
@@ -11,7 +11,7 @@ sudo pacman -S --noconfirm \
 xorg-server \
 xorg-xinit \
 noto-fonts noto-fonts-emoji noto-fonts-extra noto-fonts-cjk \
-ttf-fira-code
+ttf-fira-code \
 # openbox \
 obconf \
 feh \
